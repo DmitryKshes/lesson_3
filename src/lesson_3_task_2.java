96333bf2fba3d03294d82669603126f6255900a0
DmitryKshes <dmitriykshes@gmail.com>
2024-09-06 17:04:56 +0300

    init: complete first and second tasks

--- src/lesson_3_task_2.java
+++ src/lesson_3_task_2.java
@@ -16,15 +16,28 @@ public class lesson_3_task_2 {
         System.out.println("Array is: " + Arrays.toString(array));
         System.out.println("Enter value for array: ");
         int enterValue = new Scanner(System.in).nextInt();
-
+        int newSizeOfArray = 0;
         for (int i : array) {
             if (enterValue == i) {
+                newSizeOfArray++;
                 System.out.println(i);
             }
         }
-        System.out.println(Arrays.toString(array));
-
+        newSizeOfArray = array.length - newSizeOfArray;
+        if (newSizeOfArray == 0)
+            System.out.println(Arrays.toString(array));
+        else {
+            int[] newArray = new int[newSizeOfArray];
+            int indexInNewArray = 0;
+            for (int i = 0; i < array.length; i++) {
+                if (array[i] != enterValue) {
+                    newArray[indexInNewArray] = array[i];
+                    indexInNewArray++;
+                }
             }
-
-
+            System.out.println("Array without value: " + Arrays.toString(newArray));
         }
+    }
+}
+
+
